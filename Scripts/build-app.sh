415c1812480cf3d187860acd9f45bc92811b74f6
Michell Zappa <mz@envisioning.com>
2026-09-12 16:05:29 +0200

    Clear Finder metadata before signing app

--- Scripts/build-app.sh
+++ Scripts/build-app.sh
@@ -17,6 +17,7 @@ cp "$projectDirectory/Resources/Cargo-Info.plist" "$appDirectory/Contents/Info.p
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $CARGO_VERSION" "$appDirectory/Contents/Info.plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$appDirectory/Contents/Info.plist"
 chmod +x "$appDirectory/Contents/MacOS/Cargo"
+/usr/bin/xattr -cr "$appDirectory"
 codesign --force --deep --sign - --timestamp=none "$appDirectory"
 
 printf '%s (version %s, build %s)\n' "$appDirectory" "$CARGO_VERSION" "$buildNumber"
